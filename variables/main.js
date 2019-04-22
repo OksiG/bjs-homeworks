6a1599714c88190b18821ffb1d0c3feeb68b87c4
OksiG <okcana693@mail.ru>
2019-04-22 22:36:22 +0300

    third task

--- variables/main.js
+++ variables/main.js
@@ -9,3 +9,10 @@ console.log(averageGrade);
 let name = 'Oksana';
 let message = `Привет, мир! Меня зовут ${name}`;
 console.log(message);
+
+//Задача 3
+let x = 2;
+let y = 22;
+let z = 0;
+let result = x * y + 5 * z + x - 1;
+console.log(result);
