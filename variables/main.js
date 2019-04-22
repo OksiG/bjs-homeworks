9d17753ce037a588fd89f58fd760346f0208c1be
OksiG <okcana693@mail.ru>
2019-04-22 22:30:59 +0300

    second task

--- variables/main.js
+++ variables/main.js
@@ -1,5 +1,11 @@
+//Задача 1
 let algebra = 5;
 let geography = 4;
 let physics = 4;
 let averageGrade = (algebra + geography + physics) / 3;
 console.log(averageGrade);
+
+//Задача 2
+let name = 'Oksana';
+let message = `Привет, мир! Меня зовут ${name}`;
+console.log(message);
