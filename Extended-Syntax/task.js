bc2b8e87c4f23cb96f495bc99037632d0efa22cf
OksiG <okcana693@mail.ru>
2019-04-23 22:23:32 +0300

    first task

--- Extended-Syntax/task.js
+++ Extended-Syntax/task.js
@@ -13,6 +13,19 @@ function calculateQuadraticEquation(){
 function getResult(a,b,c){
     // код для задачи №1 писать здесь
     //return x;
+    "use strict";
+    discriminant = Math.pow(b, 2) - 4 * a * c;
+    if (discriminant < 0) {
+        console.log('Корней нет');
+    } else if (discriminant === 0) {
+        x = -b / (2 * a);
+        console.log(x);
+        return x;
+    } else if (discriminant > 0) {
+        x1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+        x2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+        console.log(x1, x2);
+    }
 }
 
 function calculateDrinkTask(){
