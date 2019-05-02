0a7f5e222277497ba7d3e4a6f8f6bdd7d5efdd72
OksiG <okcana693@mail.ru>
2019-05-02 18:36:12 +0300

    first commit wrappers-for-primitives

--- wrappers-for-primitives/task.js
+++ wrappers-for-primitives/task.js
@@ -13,7 +13,31 @@ function calculateTotalMortgage(percent, contribution, amount, date) {
 
     // код для задачи №1 писать здесь
     //return totalAmount;
-}
+    'use strict';
+    function isNumeric(n) {
+        return !isNaN(parseFloat(n)) && isFinite(n);
+      }
+    if (isNumeric (percent) === false) {
+        console.log('Данные указаны не верно! Введите процент заново');
+    } else if (percent > 100) {
+        console.log('Данные указаны не верно! Процентная ставка не может быть более 100%')
+    }  else if (isNumeric (contribution) === false) {
+        console.log('Данные указаны не верно! Введите первоночальный взнос заново');
+    } else if (isNumeric (amount) === false) {
+        console.log('Данные указаны не верно! Введите сумму кредита заново');
+    } else if (isNumeric (date) === false) {
+        console.log('Данные указаны не верно! Введите срок кредита заново');
+    } 
+
+    let AmountWithoutContribution = amount - contribution;
+    let p = percent / 100 / 12;
+    let payMounth = AmountWithoutContribution * (p + p / ((Math.pow( (1 + p), date)) - 1));
+    let totalAmount = payMounth * date;
+    console.log(Math.ceil(totalAmount * 100) / 100);
+    return totalAmount;
+    }
+calculateTotalMortgage (10, 1000, 50000, 12);
+
 
 function sayHello() {
     let name = window.personName.value;
@@ -25,4 +49,17 @@ function sayHello() {
 function getGreeting(name) {
     // код для задачи №2 писать здесь
     //return greeting;
+    'use strict';
+    let greeting = name;
+    if ( name === '') {
+        name = 'Аноним';
+    } else if ( name === undefined) {
+        name = 'Аноним';
+    } else if ( name === null) {
+        name = 'Аноним';
+    }
+
+    console.log(`Привет, мир! Меня зовут ${name}`);
+    return greeting;
 }
+getGreeting('');
