6f302999cd58c0d3650d91b94138c182a52a3372
OksiG <okcana693@mail.ru>
2019-05-20 20:59:40 +0300

    correct setAlarm

--- async/main.js
+++ async/main.js
@@ -1,15 +1,34 @@
 function setDailyRhythm(wakeUpTime, bedTime) {
-    function setAlarm(time, callback) {
-        return function () {
-            let data = new Date ();
-            Hour = data.getHours();
-            Minutes = data.getMinutes();
-            let a = Hour + ':' + Minutes;
-            console.log(a);
-            if ( a === time ) {
-                return callback();
-            }
+    
+}
+
+function setAlarm(time, callback) {
+    return function (data) {
+        let hour = data.getHours();
+        let minutes = data.getMinutes();
+        let a = hour + ':' + minutes;
+
+        if (hour < 10 && minutes < 10) {
+            hour = '0' + hour;
+            minutes = '0' + minutes;
+        }
+        
+        console.log(a);
+
+        if ( a === time ) {
+            return callback();
         }
     }
-
 }
+
+const goodMorning = () => alert('Доброе утро!'),
+	checkTime = setAlarm('07:00', goodMorning);
+
+/*
+	передаём в checkTime информацию о 
+	текущем времени системы. Так как 07:00 и 07:30,
+	goodMorning не вызывается
+*/
+checkTime('07:30');
+
+checkTime('07:00'); // Доброе утро
