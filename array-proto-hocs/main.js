ced9c9999c7f3e270927ff3cd10cf04ca07234f1
OksiG <okcana693@mail.ru>
2019-05-28 22:58:24 +0300

    finish commit

--- array-proto-hocs/main.js
+++ array-proto-hocs/main.js
@@ -1,24 +1,10 @@
 //Задание 1
 
 function compareArrays( arr1, arr2 ) {
-    if (arr1.length !== arr2.length) {
-        return false;
-    } else if (arr1.length === arr2.length) {
-        checkMassiv(arr1, arr2);
-        return true;
-    }
-
-    function checkMassiv (arr1, arr2) {
-        for (let i = 0; i < arr1.length; i++) {
-            if (arr1[i] === arr2 [i]) {
-                return true;
-            } else {
-                return false;
-            }
-        }
-    }
-}
-
+    return arr1.length !== arr2.length
+      ? false
+      : arr1.every((n, i) => n === arr2[i]);
+};
 
 compareArrays([8, 9], [6]); // false, разные значения
 compareArrays([8, 9, 5, 4], [8, 9, 5, 4, 8, 3, 5]); // false, разные значения
@@ -26,29 +12,43 @@ compareArrays([9, 2, 4, 8, 2], [9, 2, 4]); // false, разные значени
 compareArrays([1, 2, 3], [2, 3, 1]); // false, разные индексы, хотя и одинаковые значения
 compareArrays([8, 1, 2], [8, 1, 2]); // true
 
-function memoize(fn, limit) {
-    const results = [];
+function memoize (fn, limit) {
+    let results = [];
 
-    for (result in results) {
+    return function (arg1, arg2) {
+        const args = [arg1, arg2];
 
-        results[i] = {
-            args,
-            result
-        }
-        
-    }
-    return mSum;
+        if (results.length === 0) {
+            results.push({
+                args,
+                result: fn (arg1, arg2),
+            });
+            return fn (arg1, arg2);
+        } else {
+            const search = results.find(element => compareArrays(element.args, args));
+
+            if (search) {
+                console.log('Результат находится в памяти');
+                return search.result;
+            } else {
+                let result = fn (arg1, arg2);
+                results.push ({
+                    args,
+                    result
+                });
+
+                if (results.length > limit) {
+                    results.shift();
+                }
+                return result;
+            };
+        };
+    };
+};
+
+const fn = (a, b) => {
+    console.log('Результат не находится в памяти');
+    return a + b;
 }
 
-const sum = (a, b) => a + b;
-
-const mSum = memoize(sum, 2); // 2 результата хранятся в памяти
-
-// Вызов этих функций даёт один и тот же результат
-sum( 3, 4 ); // 7
-/* 
-  разница только в том, что mSum запоминает результат (7)
-  и повторно не делает вычисления
- */
-mSum( 3, 4 ); // 7
-
+const mSum = memoize( fn, 10 );
